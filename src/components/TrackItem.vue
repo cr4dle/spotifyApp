b2528aadcdc4ce28ca89f41bcf2d1eca82742f10
cr4dle <pacofernandez1986@gmail.com>
2022-02-20 21:43:56 +0000

    Display preview song

--- src/components/TrackItem.vue
+++ src/components/TrackItem.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="track">
+  <div
+    :class="['track', { 'play-sample': track.preview_url }]"
+    @click="onClick"
+  >
     <card>
       <slot name="header">
         <img class="avatar" :src="albumCover" />
       </slot>
       <slot>
         <span>{{ track.name }}</span>
+        <template v-if="autoplay">
+          <iframe
+            ref="player"
+            :src="track.preview_url"
+            frameborder="0"
+            allowtransparency="true"
+            allow="encrypted-media"
+          ></iframe>
+        </template>
+        <!-- <iframe
+          class="test-iframe"
+          ref="player"
+          :src="playerUrl"
+          height="80px"
+          frameborder="0"
+          allowtransparency="true"
+          allow="encrypted-media"
+        ></iframe> -->
       </slot>
     </card>
   </div>
@@ -13,8 +34,9 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
-import { Track } from "@/types/RecentlyPlayedTracksResponse";
+
 import Card from "@/components/Card.vue";
+import { Track } from "@/types/common/Track";
 
 @Component({
   components: {
@@ -24,9 +46,19 @@ import Card from "@/components/Card.vue";
 export default class TrackItem extends Vue {
   @Prop({ required: true }) private track!: Track;
 
+  private autoplay: boolean = false;
+
   get albumCover() {
     // TODO there are different image size. There could be some logic based on screen resolution to get the best fit
     return this.track.album.images[0].url;
+  }
+
+  get playerUrl(): string {
+    return `https://open.spotify.com/embed/track/${this.track.id}`;
+  }
+
+  onClick() {
+    this.autoplay = !this.autoplay;
   }
 }
 </script>
@@ -36,6 +68,13 @@ export default class TrackItem extends Vue {
   margin: 10px;
   flex-grow: 1;
   display: flex;
+  pointer-events: none;
+  min-width: 300px;
+
+  &.play-sample {
+    pointer-events: all;
+    cursor: pointer;
+  }
 
   .avatar {
     width: 100%;
